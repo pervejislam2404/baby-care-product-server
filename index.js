1c1d56630f33a6a21170b5fe0e744bdcf3292614
pervejislam2404 <pervejislam2404@gmail.com>
2021-11-12 16:18:20 +0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var admin = require("firebase-admin");
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
@@ -7,9 +8,39 @@ const ObjectID = require('mongodb').ObjectID;
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
+
+// baby-care-products-50b0d-firebase-adminsdk.json
+
+var serviceAccount = require("./baby-care-products-50b0d-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+// admin
+
+
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvyqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// token-check
+
+async function verifyToken(req,res,next){
+if(req?.headers?.authorization.startsWith('Bearer ')){
+    const token = req.headers?.authorization.split(' ')[1]
+   
+    try{
+       const decodedUser = await admin.auth().verifyIdToken(token);
+       req.decodedEmail= decodedUser.email;
+    }
+    catch{
+
+    }
+}
+
+
+    next()
+}
 
 async function run() {
     try {
@@ -59,13 +90,17 @@ async function run() {
         })
        
 
-        // make-admin-with-email
-        app.put('/makeAdmin/:email', async (req,res)=>{
+        // make-admin-with-email-and-verify-user-with-jwt 
+        app.put('/makeAdmin/:email',verifyToken, async (req,res)=>{
             const email = req.params.email;
-            const query = {email: email};
-            const updateDocs = {$set:{role: 'admin'}};
-            const result = await usersCollection.updateOne(query,updateDocs);
-            res.send(result)
+           if(req?.decodedEmail){
+               const query = {email: email};
+               const updateDocs = {$set:{role: 'admin'}};
+               const result = await usersCollection.updateOne(query,updateDocs);
+               res.send(result)
+           }else{
+            res.json({})
+           }
         })
 
         // get-every-single-product-details
@@ -91,19 +126,27 @@ async function run() {
             res.json(result)
         })
 
-        // delete-an-order
-        app.delete('/deleteOrder/:id', async (req,res)=>{
+        // delete-an-order-and-verify-user-with-jwt
+        app.delete('/deleteOrder/:id',verifyToken, async (req,res)=>{
             const id = req.params.id;
+            if(req?.decodedEmail){
             const query = {_id: ObjectID(id)};
             const result = await orderCollection.deleteOne(query);
             res.json(result);
+            }else{
+                res.json({})
+            }
         })
 
-        // get-all-users-order
+        // get-all-users-order-and-verify-with-jwt-token
         app.get('/getAllOrders', async (req,res)=>{
+            if(req?.decodedEmail){
             const query = {};
             const result = await orderCollection.find(query).toArray();
             res.json(result);
+            }else{
+                res.json({})
+            }
         })
 
         // get-all-main-products
@@ -114,46 +157,67 @@ async function run() {
         })
 
 
-        // updating-the-product-status
+        // updating-the-product-status-and-verify-user-with-jwt
         app.put('/setStatus/:id', async (req,res)=>{
-            const id = req.params.id;
-            const query = {_id: ObjectID(id)};
-            const updateDocs ={$set:{status: 'shipped'}};
-            const result = await orderCollection.updateOne(query,updateDocs);
-            res.json(result); 
+            if(req?.decodedEmail){
+                const id = req.params.id;
+                const query = {_id: ObjectID(id)};
+                const updateDocs ={$set:{status: 'shipped'}};
+                const result = await orderCollection.updateOne(query,updateDocs);
+                res.json(result); 
+            }else{
+                res.json({})
+            }
         })
 
 
-        // delete-the-main-product
+        // delete-the-main-product-and-verify-user-with-jwt
         app.delete('/deleteProduct/:id', async (req,res)=>{
-            const id = req.params.id;
-            console.log(id)
-            const query = {_id: ObjectID(id)};
-            const result = await productCollection.deleteOne(query);
-            res.json(result);
+            if(req?.decodedEmail){
+                const id = req.params.id;
+                console.log(id)
+                const query = {_id: ObjectID(id)};
+                const result = await productCollection.deleteOne(query);
+                res.json(result);
+            }else{
+                res.json({})
+            }
         })
 
 
-        // add-a-product-to-main-product
+        // add-a-product-to-main-product-and-verify-user-with-jwt
         app.post('/addProduct', async (req,res)=>{
-            const product = req.body;
-            const result = await productCollection.insertOne(product);
-            res.json(result);
+            if(req?.decodedEmail){
+                const product = req.body;
+                const result = await productCollection.insertOne(product);
+                res.json(result);
+            }else{
+                res.json({})
+            }
         })
 
-        // save-user-review-to-database
+        // save-user-review-to-database-and-verify-user-with-jwt
         app.post('/saveReview', async (req,res)=>{
+            if(req?.decodedEmail){
             const review = req.body;
-           const result = await reviewCollection.insertOne(review);
-           res.json(result);
+            const result = await reviewCollection.insertOne(review);
+            res.json(result);
+            }else{
+                res.json({})
+            }
         })
 
-        // get-all-users-review
+        // get-all-users-review-and-verify-user-with-jwt
         app.get('/getAllReview', async (req,res)=>{
-            const query = {};
-            const result = await reviewCollection.find(query).toArray();
-            res.json(result);
+            if(req?.decodedEmail){
+                const query = {};
+                const result = await reviewCollection.find(query).toArray();
+                res.json(result);
+            }else{
+                res.json({})
+            }
         })
+
 
     } finally {
         // await client.close();
